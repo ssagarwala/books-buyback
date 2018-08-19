5e9827bf7cd7b89b116c1a4d0de58e88d30760e3
Sehar <sfsial@gmail.com>
2018-08-19 00:41:21 -0500

    login controller, user.java and registration.html modified

--- src/main/java/com/capstone/booksbuyback/model/User.java
+++ src/main/java/com/capstone/booksbuyback/model/User.java
@@ -13,7 +13,6 @@ import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
-import com.capstone.booksbuyback.model.Role;
 import org.hibernate.validator.constraints.Email;
 import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.NotEmpty;
